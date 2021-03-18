9629f4fc0852d7d5968a267487ced6a06554badf
SuedaOguzalp <Sueda.Oguzalp@intertech.com.tr>
2021-03-18 22:09:49 +0300

    update login page

--- src/views/login-page/login-page.js
+++ src/views/login-page/login-page.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./loginpage.scss";
 
 const LoginPage = () => {
-	return <div></div>;
+	return <div>
+		<h2> hello </h2>
+	</div>;
 };
 
 export default LoginPage;
