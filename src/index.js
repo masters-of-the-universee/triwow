40caf6fd8f096241185df485d0cabe0d4734d7ac
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-20 13:36:19 +0300

    Add provider wrapper to app.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  rootElement
 );
