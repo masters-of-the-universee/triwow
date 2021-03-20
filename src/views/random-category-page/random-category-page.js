f1a7c78fab9a96647fd76df0984ee2258b33ae8d
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-20 22:39:19 +0300

    Add countdown functionality and update useeffects

--- src/views/random-category-page/random-category-page.js
+++ src/views/random-category-page/random-category-page.js
@@ -4,29 +4,52 @@ import circlePng from '../../assets/circle.svg';
 
 export default function RandomCategoryPage() {
   const [rotateDeg, setRotateDeg] = useState(0);
+  const [countdownStarted, setCountdownStarted] = useState(true);
+  const [countdown, setCountdown] = useState(3);
 
   function getRandomDegree() {
     const degree = parseInt(Math.random() * (1900 - 600) + 600);
     const winner = degree % 360;
     const winIndex = Math.ceil(winner / 60);
+    console.log(degree, winIndex); // for debug
     return { degree, winIndex };
   }
 
   useEffect(() => {
-    const deg = getRandomDegree();
-    setRotateDeg(deg.degree);
+    setCountdownStarted(true);
   }, []);
+
+  useEffect(() => {
+    if (countdownStarted === false) {
+      const deg = getRandomDegree();
+      setRotateDeg(deg.degree);
+    }
+  }, [countdownStarted]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (countdown <= 1) {
+        return setCountdownStarted(false);
+      }
+      setCountdown(countdown - 1);
+    }, 1000);
+  }, [countdown]);
+
+  const transformRotateStyle = rotateDeg
+    ? {
+        transform: `rotate(-${rotateDeg}deg)`,
+      }
+    : null;
 
   return (
     <div>
-      <h2>RandomCategoryPage</h2>
-      <img
-        id="circle"
-        style={rotateDeg ? { transform: `rotate(-${rotateDeg}deg)` } : null}
-        src={circlePng}
-        alt="circle"
-      />
-      <input type="text" />
+      {countdownStarted ? (
+        <div className="countdown">
+          <div className="countdown-container">{countdown}</div>
+        </div>
+      ) : (
+        <img id="circle" style={transformRotateStyle} src={circlePng} alt="circle" />
+      )}
     </div>
   );
 }
