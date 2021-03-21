78451508989a5fabd768d5174eeecc2ec4a1434a
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-21 18:49:45 +0300

    Fix for late transition problem

--- src/views/random-category-page/random-category-page.js
+++ src/views/random-category-page/random-category-page.js
@@ -3,53 +3,31 @@ import React, { useEffect, useState } from 'react';
 import circlePng from '../../assets/circle.svg';
 
 export default function RandomCategoryPage() {
-  const [rotateDeg, setRotateDeg] = useState(0);
-  const [countdownStarted, setCountdownStarted] = useState(true);
-  const [countdown, setCountdown] = useState(3);
+  const [rotateDeg, setRotateDeg] = useState(undefined);
 
   function getRandomDegree() {
     const degree = parseInt(Math.random() * (1900 - 600) + 600);
     const winner = degree % 360;
     const winIndex = Math.ceil(winner / 60);
-    console.log(degree, winIndex); // for debug
     return { degree, winIndex };
   }
 
   useEffect(() => {
-    setCountdownStarted(true);
+    setTimeout(() => {
+      const degree = getRandomDegree();
+      setRotateDeg(degree.degree);
+      console.log(degree);
+    }, 200);
   }, []);
 
-  useEffect(() => {
-    if (countdownStarted === false) {
-      const deg = getRandomDegree();
-      setRotateDeg(deg.degree);
-    }
-  }, [countdownStarted]);
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (countdown <= 1) {
-        return setCountdownStarted(false);
-      }
-      setCountdown(countdown - 1);
-    }, 1000);
-  }, [countdown]);
-
-  const transformRotateStyle = rotateDeg
-    ? {
-        transform: `rotate(-${rotateDeg}deg)`,
-      }
-    : null;
-
+  const transformRotateStyle = {
+    transform: `rotate(-${rotateDeg}deg)`,
+  };
   return (
-    <div>
-      {countdownStarted ? (
-        <div className="countdown">
-          <div className="countdown-container">{countdown}</div>
-        </div>
-      ) : (
-        <img id="circle" style={transformRotateStyle} src={circlePng} alt="circle" />
-      )}
-    </div>
+    <section class="category">
+      <div className="rotate" style={transformRotateStyle}>
+        <img id="circle" src={circlePng} alt="circle" />
+      </div>
+    </section>
   );
 }
