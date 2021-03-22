c5ebdb2158b07c0645cf3e90a4c4a676de5f6157
onurozdemir26 <onurozdemiroffical@gmail.com>
2021-03-22 21:30:32 +0300

    Update ModePage

--- src/views/mode-select-page/mode-select-page.js
+++ src/views/mode-select-page/mode-select-page.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './modeselectpage.scss';
 
-export default function ModeSelectPage() {
+
+function ModeSelectPage() {
+
+  const modeClick = (e) => {
+
+    console.log(e.modeClick)
+  }
+
+  useEffect(() => {
+    // console.log(modeClick)
+  }, [])
+
+
+
   return (
     <div>
-      <h2>Mode Select Page</h2>
+      <div className="navbar">
+        <h2>Mode Page</h2>
+      </div>
+      <div className="buttons">
+        <button className="button" onClick={modeClick}>Mod1</button>
+        <button className="button" onClick={modeClick}>Mod2</button>
+        <button className="button" onClick={modeClick}>Mod3</button>
+      </div>
     </div>
   );
 }
+
+export default ModeSelectPage;
