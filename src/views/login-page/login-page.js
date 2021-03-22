95555e3239ab8d56f10412437cdc70f99b783d54
Eray Durak <eraydurak1@outlook.com>
2021-03-22 19:18:58 +0300

    Worked on responsive design

--- src/views/login-page/login-page.js
+++ src/views/login-page/login-page.js
@@ -21,29 +21,34 @@ const LoginPage = ({ addingNewUsername }) => {
 	};
 
 	return (
-		<div className="login-container">
-			<div className="left-section">
-				<img src={circlePng} alt="carkifelek" />
-			</div>
-			<div className="rigth-section">
-				<h3>Write your name to start !</h3>
-				<div className="input-area">
-					<div className="wrapper">
-						<div className="input-data">
-							<input
-								onChange={onChangeHandler}
-								name="name"
-								type="text"
-								autoComplete="off"
-								required
-							/>
-							<label>Write your Name</label>
+		<div className="main-wrapper">
+			<div className="login-container">
+				<div className="left-section">
+					<img src={circlePng} alt="carkifelek" />
+				</div>
+				<div className="right-section">
+					<h3>Write your name to start !</h3>
+					<div className="input-area">
+						<div className="wrapper">
+							<div className="input-data">
+								<input
+									onChange={onChangeHandler}
+									name="name"
+									type="text"
+									autoComplete="off"
+									required
+								/>
+								<label>Write your Name</label>
+							</div>
 						</div>
 					</div>
+					<Link onClick={onClickHandler} to="/mode-selection">
+						Play
+					</Link>
+					<Link onClick={onClickHandler} to="/mode-selection">
+						Play as Anonymus
+					</Link>
 				</div>
-				<Link onClick={onClickHandler} to="/mode-selection">
-					Play
-				</Link>
 			</div>
 		</div>
 	);
