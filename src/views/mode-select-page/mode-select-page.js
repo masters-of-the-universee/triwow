6e13b42d14aaa0928b0c05d6c66380893ef31086
onurozdemir26 <onurozdemiroffical@gmail.com>
2021-03-23 21:32:24 +0300

    Update Mode-Page

--- src/views/mode-select-page/mode-select-page.js
+++ src/views/mode-select-page/mode-select-page.js
@@ -9,9 +9,13 @@ const ModeSelectPage = ({ value }) => {
         <h2>Mode Page</h2>
       </div>
       <div className="buttons">
-        <button className="button">Mod1</button>
-        <button className="button">Mod2</button>
-        <button className="button">Mod3</button>
+        <button className="button">Easy <span className="tooltiptext">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt numquam recusandae, ad consequatur atque earum alias cupiditate eum esse, tempora dolores molestias labore hic? Cumque.</span> </button>
+        <button className="button">Medium <span className="tooltiptext">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nobis aperiam adipisci earum ipsum quia ut ad fugit accusantium sint tempora, aspernatur necessitatibus quam ratione?</span> </button>
+        <button className="button">Hard
+        <span className="tooltiptext">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquid natus consequuntur voluptatem voluptatibus id ab blanditiis cupiditate autem, saepe quisquam accusamus necessitatibus eius consectetur!</span> </button>
       </div>
     </div>
   );
