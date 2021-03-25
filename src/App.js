fac20882eb0e89faad6b285861e691cf940542c4
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-25 20:59:52 +0300

    App.js update for get connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import { Link, Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import RandomCategory from "./views/random-category-page/random-category-page";
-import ModeSelectionPage from "./views/mode-select-page/mode-select-page";
-import LoginPage from "./views/login-page/login-page";
+import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import './App.css';
+import RandomCategory from './views/random-category-page/random-category-page';
+import ModeSelectionPage from './views/mode-select-page/mode-select-page';
+import LoginPage from './views/login-page/login-page';
+import getUsers from './firebase/index';
 
 function App() {
-	return (
-		<section>
-			<Router>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/mode-selection">Mode-selection</Link>
-						</li>
-						<li>
-							<Link to="/random-category">Random category</Link>
-						</li>
-					</ul>
-				</nav>
-				<main>
-					<Switch>
-						<Route path="/random-category">
-							<RandomCategory />
-						</Route>
-						<Route path="/mode-selection">
-							<ModeSelectionPage />
-						</Route>
-						<Route path="/">
-							<LoginPage />
-						</Route>
-					</Switch>
-				</main>
-			</Router>
-		</section>
-	);
+  useEffect(() => {
+		getUsers();
+  }, []);
+  return (
+    <section>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/mode-selection">Mode-selection</Link>
+            </li>
+            <li>
+              <Link to="/random-category">Random category</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/random-category">
+              <RandomCategory />
+            </Route>
+            <Route path="/mode-selection">
+              <ModeSelectionPage />
+            </Route>
+            <Route path="/">
+              <LoginPage />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </section>
+  );
 }
 
 export default App;
