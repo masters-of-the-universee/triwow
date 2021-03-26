59509bc52698a0da9c5777385f3aeb5fbd9d36cd
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-26 23:04:04 +0300

    Questions view initial commit

---
+++ src/views/questions/index.js
@@ -0,0 +1,13 @@
+import React, {} from 'react';
+import Question from '../../components/question/question'
+
+const QuestionsPage = function() {
+  return (
+    <div>
+      <h1>Questions page</h1>
+      <Question></Question>
+    </div>
+  )
+}
+
+export default QuestionsPage;
