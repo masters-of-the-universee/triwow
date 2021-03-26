4195ae1268efbb91cb38d630eeb1a461c69c32d5
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-26 23:04:17 +0300

    Question component initial commit

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,8 +1,20 @@
-import React from "react";
-import "./question.scss";
+import React from 'react';
+import './question.scss';
 
-const question = () => {
-	return <div></div>;
+const question = ({question}) => {
+  return (
+    <div className="question-card">
+      <h3>Header</h3>
+      <p>Body</p>
+      <div className="question-card-answers">
+        <ul>
+          <li>Answer1</li>
+          <li>Answer1</li>
+          <li>Answer1</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default question;
