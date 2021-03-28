4d02f7e22db92b32f91b30d4b4d74821f8b007b2
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-28 19:53:20 +0300

    Add loading action to question component

--- src/views/questions/index.js
+++ src/views/questions/index.js
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Question from '../../components/question/question';
 import Checkbox from '../../components/checkbox/index';
+import Loader from '../../components/loader/index';
 import './index.scss';
 
 const QuestionsPage = function ({ match, ...props }) {
   const [questions, setQuestions] = useState([]);
   const [questionOrder, setQuestionOrder] = useState(0);
   const [answers, setAnswers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function getQuestions(category) {
+    setIsLoading(true);
     const response = await fetch(`https://opentdb.com/api.php?amount=5&category=${category}`);
     const data = await response.json();
     console.log(data);
+    setIsLoading(false);
     setQuestions(data.results);
   }
 
@@ -37,7 +41,11 @@ const QuestionsPage = function ({ match, ...props }) {
           handleAnswersStat={handleAnswersStat}
           question={questions[questionOrder]}
         ></Question>
-      ) : null}
+      ) : (
+        <div className="loader-container">
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
