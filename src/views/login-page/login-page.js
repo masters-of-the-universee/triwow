12ccb61c2b62dc0c1b65c794b89068ebf350ddf4
Eray Durak <eraydurak1@outlook.com>
2021-04-04 14:20:03 +0300

    Added buttons and changes on responsiveness

--- src/views/login-page/login-page.js
+++ src/views/login-page/login-page.js
@@ -5,6 +5,15 @@ import circlePng from '../../assets/circle.png';
 import { connect } from 'react-redux';
 import { addingNewUsername } from './../../actions/index';
 import Database from '../../firebase/index';
+import audioPng from '../../assets/audio.png';
+import audiooffPng from '../../assets/audiooff.png';
+import qmarkPng from '../../assets/question-mark.png';
+import sound from "../../assets/music.mp3";
+import swal from 'sweetalert';
+
+
+let count = 0;
+let imgs = audiooffPng;
 
 const LoginPage = ({ addingNewUsername }) => {
   const [newName, setNewName] = useState('');
@@ -16,6 +25,22 @@ const LoginPage = ({ addingNewUsername }) => {
   useEffect(() => {
     console.log(newName);
   }, [newName]);
+
+  const playAudio = new Audio(sound);
+
+  const playSound = audioFile => {
+    if (count % 2 === 0) {
+      audioFile.play();
+      document.getElementById("imageID").src = audioPng;
+    }
+    if (count % 2 === 1) {
+      audioFile.pause();
+      audioFile.currentTime = 0;
+      document.getElementById("imageID").src = audiooffPng;
+
+    }
+    count = count + 1
+  }
 
   const onClickHandler = async () => {
     addingNewUsername(newName);
@@ -31,6 +56,11 @@ const LoginPage = ({ addingNewUsername }) => {
           <img src={circlePng} alt="carkifelek" />
         </div>
         <div className="right-section">
+          <div className="music-section">
+            <button className="music-button btnbtn" onClick={() => playSound(playAudio)}>  <img className="audioPhoto" id="imageID" src={imgs} alt="audioPhoto" /></button>
+            <button className="tutorial btnbtn" onClick={() => swal("TUTORIAL", "Oyun şöyle böyle oynanır")}>
+              <img className="tutorialPhoto" src={qmarkPng} alt="qmarkpng" /></button>
+          </div>
           <h3>Write your name to start !</h3>
           <div className="input-area">
             <div className="wrapper">
