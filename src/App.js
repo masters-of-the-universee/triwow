94c980424987765383b7344069cf64c2eed72fe8
Eray Durak <eraydurak1@outlook.com>
2021-04-06 21:41:03 +0300

    music button positioned same corner of all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-import RandomCategory from './views/random-category-page/random-category-page';
-import Questions from './views/questions/index';
-import ModeSelectionPage from './views/mode-select-page/mode-select-page';
-import LoginPage from './views/login-page/login-page';
+import { Link, Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import RandomCategory from "./views/random-category-page/random-category-page";
+import Questions from "./views/questions/index";
+import ModeSelectionPage from "./views/mode-select-page/mode-select-page";
+import LoginPage from "./views/login-page/login-page";
+import audioPng from "./assets/audio.png";
+import audiooffPng from "./assets/audiooff.png";
+import sound from "./assets/music.mp3";
+
+let count = 0;
+let imgs = audiooffPng;
 
 function App() {
+  const playAudio = new Audio(sound);
+  const playSound = (audioFile) => {
+    if (count % 2 === 0) {
+      audioFile.play();
+      document.getElementById("imageID").src = audioPng;
+    }
+    if (count % 2 === 1) {
+      audioFile.pause();
+      audioFile.currentTime = 0;
+      document.getElementById("imageID").src = audiooffPng;
+    }
+    count = count + 1;
+  };
+
   return (
     <section>
       <Router>
@@ -22,6 +42,17 @@ function App() {
           </ul>
         </nav>
         <main>
+          <button
+            className="music-button btnbtn"
+            onClick={() => playSound(playAudio)}>
+            {" "}
+            <img
+              className="audioPhoto"
+              id="imageID"
+              src={imgs}
+              alt="audioPhoto"
+            />
+          </button>
           <Switch>
             <Route path="/questions/:categoryId">
               <Questions />
