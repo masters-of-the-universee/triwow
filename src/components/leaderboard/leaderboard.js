02dbd0e8aa0250905965c151b179771f03c80f7d
Kaan Ersoy <kaanersoy12@gmail.com>
2021-04-09 00:34:14 +0300

    Update Leaderboard for mapping users and me data's

--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -1,6 +1,23 @@
-import "./leaderboard.scss";
+import React, { useEffect, useState } from 'react';
+import './leaderboard.scss';
+import Database from '../../firebase/index';
+import Loader from '../loader/index'
 
-const Leaderboard = () => {
+const Leaderboard = ({user}) => {
+  const [topTen, setTopTen] = useState(null);
+  const [me, setMe] = useState(null);
+
+  useEffect(async () => {
+    const db = new Database();
+    setTimeout(async () => {
+      const yeyTopten = await db.getTopTen()
+      setTopTen(yeyTopten);
+      const myProfile = await db.getMe(user.id);
+      setMe(myProfile);
+    }, 600)
+  }, []);
+  
+  
   return (
     <div className="leaderboard-container">
       <div className="leaderboard">
@@ -13,46 +30,21 @@ const Leaderboard = () => {
               <h4>Name</h4>
               <h4>Score</h4>
             </li>
-            <li>
-              <p>Onur#15235869</p>
-              <span>948</span>
-            </li>
-            <li>
-              <p>Onur</p>
-              <span>948</span>
-            </li>
-            <li>
-              <p>Onur</p>
-              <span>948</span>
-            </li>
-            <li>
-              <p>Onur</p>
-              <span>948</span>
-            </li>
-            <li>
-              <p>Onur</p>
-              <span>948</span>
-            </li>
-            <li>
-              <p>Onur</p>
-              <span>948</span>
-            </li>
-            <li>
-              <p>Onur</p>
-              <span>948</span>
-            </li>
-            <li>
-              <p>Onur</p>
-              <span>948</span>
-            </li>
+            {topTen ? topTen.map((user, key) => (
+              <li key={key}>
+                <p>{user.username} <span className="score">#{user.id}</span></p>
+                <span>{user.score}</span>
+              </li>
+            )): <Loader size={40} />}
           </ul>
         </div>
       </div>
       <div className="your-score">
-        <h4 className="your-name">
-          Your Name <span className="your-id">#123213</span>
+        {me ? <>
+          <h4 className="your-name">
+          {me.username}<span className="your-id">#{me.id}</span>
         </h4>
-        <p className="your-last-score">30</p>
+        <p className="your-last-score">{me.score}</p></> : <Loader size={40} ></Loader>}
       </div>
     </div>
   );
